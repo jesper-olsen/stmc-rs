8a3e8edca0fa2f0d4d9b5d8510eb43f967c629d3
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-03-20 14:32:20 -0600

    initial source

--- src/bin/a0201_05.rs
+++ src/bin/a0201_05.rs
@@ -28,24 +28,23 @@ fn steb1(data: &[f64], w: &mut [f64]) -> (f64, f64) {
     (xm, xe)
 }
 
-
 fn steb2(data: &[f64], eb: &[f64], w: &mut [f64]) -> (f64, f64) {
     //C  INPUT:   DATA (GAUSSIAN), ERROR BARS  AND  (OPTIONAL) WEIGHT
-    //C           FACTORS. (IF WEIGHT FACTORS ARE NOT GIVEN: PUT   
-    //C           W(1) < 0  AND WEIGHT FACTORS WILL WE CALCULATED 
+    //C           FACTORS. (IF WEIGHT FACTORS ARE NOT GIVEN: PUT
+    //C           W(1) < 0  AND WEIGHT FACTORS WILL WE CALCULATED
     //C           FROM THE ERROR BARS EB.)
     //C  OUTPUT:  MEAN VALUE XM AND ITS ERROR BAR XE.
     //C           WEIGHT FACTORS ARE RETURNED NORMALIZED TO ONE.
 
     if w[0] < 0.0 {
         for i in 0..w.len() {
-            w[i]=1.0/eb[i].powi(2);
+            w[i] = 1.0 / eb[i].powi(2);
         }
     }
 
     let wnorm: f64 = w.iter().sum();
     for e in w.iter_mut() {
-        *e/=wnorm;
+        *e /= wnorm;
     }
 
     //weighted mean
@@ -67,26 +66,26 @@ fn steb2(data: &[f64], eb: &[f64], w: &mut [f64]) -> (f64, f64) {
 }
 
 fn main() {
-      const KMAX: usize = 10;
-      const NMAX: usize = 2usize.pow(KMAX as u32 -1);
-      let mut data = [0.0f64;KMAX];
-      let mut dat1 = [0.0f64;NMAX];
-      let mut eb = [0.0f64;KMAX];
-      let mut wght = [0.0f64;KMAX];
-      let mut rng = Marsaglia::new(12, 34, 56, 78);
-      wght[0]=-1.0;
-      for k in 0..KMAX {
-          let ndat = if k == KMAX-1 {
-              2
-          } else {
-              2usize.pow(1 + k as u32)
-          };
-          let a = &mut dat1[0..ndat];
-          for e in a.iter_mut() {
-              *e = rng.uni();
-          }
-          (data[k],eb[k]) = steb0(a);
-      }
-      let (xm,xe) = steb2(&data, &eb, &mut wght);
-      println!("UNIFORM RANDOM NUMBERS: XM={xm:.7} +/- {xe:.7}");
+    const KMAX: usize = 10;
+    const NMAX: usize = 2usize.pow(KMAX as u32 - 1);
+    let mut data = [0.0f64; KMAX];
+    let mut dat1 = [0.0f64; NMAX];
+    let mut eb = [0.0f64; KMAX];
+    let mut wght = [0.0f64; KMAX];
+    let mut rng = Marsaglia::new(12, 34, 56, 78);
+    wght[0] = -1.0;
+    for k in 0..KMAX {
+        let ndat = if k == KMAX - 1 {
+            2
+        } else {
+            2usize.pow(1 + k as u32)
+        };
+        let a = &mut dat1[0..ndat];
+        for e in a.iter_mut() {
+            *e = rng.uni();
+        }
+        (data[k], eb[k]) = steb0(a);
+    }
+    let (xm, xe) = steb2(&data, &eb, &mut wght);
+    println!("UNIFORM RANDOM NUMBERS: XM={xm:.7} +/- {xe:.7}");
 }
