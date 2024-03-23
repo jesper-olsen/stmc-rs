03bc38e0a80bf09b0bcabafb63f5312491d1bcee
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-03-22 19:21:27 -0600

    initial source

---
+++ src/marsaglia.rs
@@ -0,0 +1,126 @@
+// Rust implementation of the pseudo random number generator presented in
+// TOWARD A UNIVERSAL RANDOM NUMBER GENERATOR
+// George MARSAGLIA, Arif ZAMAN and Wai Wan TSANG
+// Statistics & Probability Letters 8 (1990) 35-39
+
+const N: usize = 97;
+use std::f64::consts::PI;
+
+pub struct Marsaglia {
+    u: [f64; N],
+    c: f64,
+    cd: f64,
+    cm: f64,
+    ip: usize,
+    jp: usize,
+    gauss: Option<f64>,
+}
+
+impl Marsaglia {
+    //C*** FIRST CALL RSTART (I, J, K, L)
+    //C*** WITH I, J, K, L INTEGERS
+    //C*** FROM 1 TO 168, NOT ALL 1
+    //C*** NOTE: RSTART CHANGES I, J, K, L
+    //C*** SO BE CAREFUL IF YOU REUSE
+    //C* * * THEM IN THE CALLING PROGRAM.
+
+    pub fn new(mut i: i32, mut j: i32, mut k: i32, mut l: i32) -> Self {
+        let mut u = [0.0; N];
+        for e in u.iter_mut().take(N) {
+            let mut s = 0.0;
+            let mut t = 0.5;
+            for _ in 0..24 {
+                let m = (((i * j) % 179) * k) % 179;
+                i = j;
+                j = k;
+                k = m;
+                l = (53 * l + 1) % 169;
+                if l * m % 64 >= 32 {
+                    s += t;
+                }
+                t *= 0.5;
+            }
+            *e = s;
+        }
+
+        Marsaglia {
+            u,
+            c: 362436. / 16777216.,
+            //cd: 7654321. / 167777216.,
+            cd: 7654321. / 16777216.,
+            cm: 16777213. / 16777216.,
+            ip: N - 1,
+            jp: 32,
+            gauss: None,
+        }
+    }
+
+    pub fn uni(&mut self) -> f64 {
+        let mut uni = self.u[self.ip] - self.u[self.jp];
+        if uni < 0.0 {
+            uni += 1.0;
+        }
+        self.u[self.ip] = uni;
+        self.ip = if self.ip == 0 { N - 1 } else { self.ip - 1 };
+        self.jp = if self.jp == 0 { N - 1 } else { self.jp - 1 };
+        self.c -= self.cd;
+        if self.c < 0.0 {
+            self.c += self.cm;
+        }
+        uni -= self.c;
+        if uni < 0.0 {
+            uni + 1.0
+        } else {
+            uni
+        }
+    }
+
+    // normally distributed random number
+    pub fn gauss(&mut self) -> f64 {
+        if let Some(r) = self.gauss {
+            self.gauss = None;
+            r
+        } else {
+            let u1 = self.uni();
+            let u2 = self.uni();
+            let r = (-2.0 * (1.0 - u1).ln()).sqrt();
+            let phi = 2.0 * std::f64::consts::PI * u2;
+            self.gauss = Some(r * phi.sin());
+            r * phi.cos()
+        }
+    }
+
+    // cauchy distributed random number
+    pub fn cauchy(&mut self) -> f64 {
+        (2.0 * PI * self.uni()).tan()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Marsaglia;
+    #[test]
+    fn it_works() {
+        let mut rng = Marsaglia::new(12, 34, 56, 78);
+        let mut l = Vec::new();
+        for ii in 1..=20005 {
+            let x = rng.uni();
+            if ii > 20000 {
+                for i in 1..=7 {
+                    let v = (x * 16.0_f64.powi(i)) as i32;
+                    let v = v % 16;
+                    l.push(v);
+                }
+            }
+        }
+        #[rustfmt::skip]
+        const R: [i32; 35] = [
+            6, 3, 11,  3,  0,  4, 0, 
+           13, 8, 15, 11, 11, 14, 0, 
+            6, 15, 0,  2,  3, 11, 0, 
+            5, 14, 2, 14,  4,  8, 0, 
+            7, 15, 7, 10, 12,  2, 0,
+        ];
+        assert!(l.iter().eq(R.iter()));
+    }
+}
---
+++ src/steb.rs
@@ -0,0 +1,90 @@
+///C  INPUT:   ARRAY X(N) WITH (GAUSSIAN) DATA.
+///C  OUTPUT:  MEAN VALUE XM, UNBIASED VARIANCE XV AND ERROR BAR XE.
+pub fn steb0(data: &[f64]) -> (f64, f64) {
+    let xm = data.iter().sum::<f64>() / data.len() as f64;
+    let xv = data.iter().map(|x| (x - xm).powi(2)).sum::<f64>() / (data.len() as f64 - 1.0);
+    let xe = (xv / (data.len() as f64)).sqrt();
+    (xm, xe)
+
+    // 1-pass
+    //    let mut xm = 0.0f64;
+    //    let mut xv = 0.0f64;
+    //    let mut n: usize = 1;
+    //    for &x in data {
+    //        n += 1;
+    //        let delta = x - xm;
+    //        xm += delta / n as f64;
+    //        xv += delta * (x - xm);
+    //    }
+    //    if n > 1 {
+    //        xv /= (n - 1) as f64; // Unbiased estimator for sample variance
+    //    } else {
+    //        xv = f64::NAN; // Handle single-element input
+    //    }
+    //
+    //    let xe = (xv / (data.len() as f64)).sqrt();
+    //    return (xm, xe);
+}
+
+///C  INPUT:   ARRAY X (GAUSSIAN) DATA AND WEIGHT FACTORS.
+///C  OUTPUT:  MEAN VALUE XM, AND ITS ERROR BAR XE.
+///C           WEIGHT FACTORS NORMALIZED TO ONE.
+pub fn steb1(data: &[f64], w: &mut [f64]) -> (f64, f64) {
+    //C  INPUT:   ARRAY X (GAUSSIAN) DATA AND WEIGHT FACTORS.
+    //C  OUTPUT:  MEAN VALUE XM, AND ITS ERROR BAR XE.
+    //C           WEIGHT FACTORS NORMALIZED TO ONE.
+    assert_eq!(data.len(), w.len());
+    let wnorm = w.iter().sum::<f64>();
+    for e in w.iter_mut() {
+        *e /= wnorm;
+    }
+    let xm = data
+        .iter()
+        .zip(w.iter())
+        .fold(0.0, |acc, (&x, &y)| acc + x * y);
+    let xv = data
+        .iter()
+        .zip(w.iter())
+        .fold(0.0, |acc, (&x, &y)| acc + y * (x - xm).powi(2));
+    let xe = (xv / (data.len() - 1) as f64).sqrt();
+    (xm, xe)
+}
+
+///C  INPUT:   DATA (GAUSSIAN), ERROR BARS  AND  (OPTIONAL) WEIGHT
+///C           FACTORS. (IF WEIGHT FACTORS ARE NOT GIVEN: PUT
+///C           W(1) < 0  AND WEIGHT FACTORS WILL WE CALCULATED
+///C           FROM THE ERROR BARS EB.)
+///C  OUTPUT:  MEAN VALUE XM AND ITS ERROR BAR XE.
+///C           WEIGHT FACTORS ARE RETURNED NORMALIZED TO ONE.
+pub fn steb2(data: &[f64], eb: &[f64], w: &mut [f64]) -> (f64, f64) {
+    if w[0] < 0.0 {
+        for (w, e) in w.iter_mut().zip(eb.iter()) {
+            *w = 1.0 / (*e).powi(2);
+        }
+        //for i in 0..w.len() {
+        //w[i] = 1.0 / eb[i].powi(2);
+        //}
+    }
+
+    let wnorm: f64 = w.iter().sum();
+    for e in w.iter_mut() {
+        *e /= wnorm;
+    }
+
+    //weighted mean
+    let xm = data
+        .iter()
+        .zip(w.iter())
+        .fold(0.0, |acc, (&x, &y)| acc + x * y);
+
+    //weighted error
+    let xe = eb
+        .iter()
+        .zip(w.iter())
+        .fold(0.0, |acc, (&x, &y)| acc + x.powi(2) * y.powi(2))
+        .sqrt();
+
+    //variance
+    //xe*f64::sqrt(data.len() as f64
+    (xm, xe)
+}
