d4f8fbd50321ac0c39978d6f72cb9c7a64ef5023
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-03-23 08:47:30 -0600

    +unit test

--- src/bin/a0202_03.rs
+++ src/bin/a0202_03.rs
@@ -1,41 +1,37 @@
-
 use marsaglia_rs::marsaglia::Marsaglia;
 use marsaglia_rs::steb::{steb0};
 use marsaglia_rs::{qtiles};
 
-/// Variance fractiles: experimental verification.
-fn main() {
+fn fractiles(q: f64) -> (f64,f64) {
     const NDAT:usize=10000;
     const ND2:usize=2;
     let mut var = [0.0f64; NDAT];
     let mut data = [0.0f64; NDAT];
     let mut rng = Marsaglia::new(12, 34, 56, 78);
-    for i in 0..NDAT {
+    for e in var.iter_mut() {
         data[0]=rng.gauss();
         data[1]=rng.gauss();
-        (_,var[i])=steb0(&data[0..ND2]);
-        var[i]=2.0*var[i].powi(2);
+        let (_,v)=steb0(&data[0..ND2]);
+        *e=2.0*v.powi(2);
     }
     var.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    qtiles(&var,q).unwrap()
+}
+
+/// Variance fractiles: experimental verification.
+fn main() {
     let q=0.15;
-    let (vq1,vq2) = qtiles(&var,q).unwrap(); 
+    let (vq1,vq2) = fractiles(q);
     println!("q: {q} vq1: {vq1} vq2: {vq2}");
 }
 
-//      PARAMETER (IUO=6,IUD=10,NDAT=10000,ND2=2,ISEED1=1,ISEED2=0)
-//      DIMENSION VAR(NDAT),DATA(ND2)
-//C
-//      CALL RMASET(IUO,IUD,ISEED1,ISEED2,'nofile')
-//      WRITE(IUO,*) ' '
-//      DO N=1,NDAT
-//        CALL RMAGAU(DATA(1),DATA(2))
-//        CALL STEB0(ND2,DATA,DM,DV,VAR(N))
-//        VAR(N)=2.0D00*VAR(N)**2
-//      END DO
-//C
-//      CALL HEAPSORT(NDAT,VAR)
-//      Q=0.15D00
-//      CALL QTILES(NDAT,VAR,Q,VQ1,VQ2)
-//      WRITE(IUO,100) Q,VQ1,VQ2
-//100   FORMAT(1X,'Q,VQ1,VQ2: ',3F18.8)
-//C
+#[cfg(test)]
+mod tests {
+    use crate::fractiles;
+    #[test]
+    fn test_fractiles() {
+        let (vq1,vq2) = fractiles(0.15);
+        assert_eq!(vq1, 0.03470057507208726);
+        assert_eq!(vq2, 2.045777357830865);
+    }
+}
