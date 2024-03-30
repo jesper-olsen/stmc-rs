b5fc419e1ea8a6f7b2e31b97692a626075e47c9b
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-03-30 16:51:05 -0600

    initial source

---
+++ src/bin/a0208_01.rs
@@ -0,0 +1,48 @@
+use marsaglia_rs::fitl::{fit_l, subl_1ox, LFit};
+
+fn main() {
+    lfit();
+}
+
+// Gaussian data: x, y, error bars
+// Linear Regression. Example 9-2 (p.201) from S. Brandt: Statistical and Computational
+// Methods in Data Analysis.
+static DATA: [(f64, f64, f64); 4] = [
+    (0.0, 1.4, 0.5),
+    (1.0, 1.5, 0.5),
+    (2.0, 3.7, 0.5),
+    (3.0, 4.1, 0.5),
+];
+
+fn lfit() -> LFit {
+    let data: Vec<(f64, f64, f64)> = DATA
+        .iter()
+        //.map(|(x, y, ey)| subl_1ox(*x, *y, *ey))
+        .map(|(x, y, ey)| (*x, *y, *ey))
+        .collect();
+
+    let r = fit_l(&data);
+    println!("{r}");
+    r
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{lfit, LFit};
+    #[test]
+    fn lfit_test() {
+        let expected = LFit {
+            a: (1.13, 1.03),
+            sga: (0.4183300132670378, 0.22360679774997896),
+            chi2: 3.1320000000000006,
+            q: 0.20887902975523354,
+            cov: [
+                [0.17500000000000002, -0.075],
+                [-0.075, 0.049999999999999996],
+            ],
+        };
+
+        let r = lfit();
+        assert_eq!(r, expected);
+    }
+}
